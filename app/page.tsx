b93c4179afe4ad6f32c56633738a65231eff6840
EmranAli-15 <mohammademranali15@gmail.com>
2026-02-02 20:21:26 +0600

    updating user info

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Footer from '@/components/Footer'
 import Banner from '../components/Banner'
 import manNew from "@/public/manNew.png"
 import Image from 'next/image'
@@ -7,12 +8,30 @@ import { useEffect, useState } from 'react'
 
 const rawData = [
   {
+    name: "Md Rafiul Hasan Rifat",
+    email: "rafiulhasanrifat2002@gmail",
+    photo: "https://i.ibb.co/x8GSmR5m/rifat1.png",
+    id: "CSE2401031096",
+    phone: "01606090890",
+    section: "31M1",
+    score: 25
+  },
+  {
     name: "Asif Karim Ornob",
     email: "ornobasifkarim@gmail.com",
     id: "CSE2401031169",
     phone: "01906428465",
     section: "31M1",
-    score: 34.6
+    score: 25
+  },
+  {
+    name: "Atkia Nawar Anisha",
+    email: "atkianawaranisha@gmail.com",
+    photo: "https://i.ibb.co/Zpb6jvN5/anisha1.png",
+    id: "CSE2401031093",
+    phone: "01755380844",
+    section: "31M1",
+    score: 25
   },
   {
     name: "Ashraful Haque",
@@ -20,23 +39,16 @@ const rawData = [
     id: "CSE2401031137",
     phone: "01635-047881",
     section: "31M1",
-    score: 34.6
+    score: 25
   },
   {
     name: "Pranto Paul",
     email: "paulpranto413@gmail.com",
+    photo: "https://i.ibb.co/VYBHL0Wc/pranto1.png",
     id: "CSE2401031089",
     phone: "01616377899",
     section: "31M1",
-    score: 38.6
-  },
-  {
-    name: "Md Rafiul Hasan Rifat",
-    email: "rafiulhasanrifat2002@gmail",
-    id: "CSE2401031096",
-    phone: "01606090890",
-    section: "31M1",
-    score: 38.6
+    score: 25
   },
   {
     name: "Ashra Siddiqa Yeantun",
@@ -44,16 +56,18 @@ const rawData = [
     id: "CSE2401031182",
     phone: "01590084318",
     section: "31M1",
-    score: 38.6
+    score: 25
   },
   {
     name: "Md.khalequzzaman",
     email: "khalequzzaman26@gmail.com",
+    photo: "https://i.ibb.co/20kNWkb2/khalequzzaman1.png",
     id: "CSE2401031091",
     phone: "01618225641",
     section: "31M1",
-    score: 38.6
+    score: 25
   },
+
 ]
 
 export default function page() {
@@ -83,7 +97,10 @@ export default function page() {
                     <div><Star w={40}></Star></div> */}
                   </div>
                 </div>
-                <Image className='h-20 w-20 md:h-35 md:w-35 object-cover absolute bottom-0' src={manNew} height={1000} width={1000} alt='me'></Image>
+                {
+                  data[1]?.photo ? <Image className='h-20 w-20 md:h-35 md:w-35 object-contain absolute bottom-0' src={data[1].photo} height={100} width={100} alt='me'></Image> :
+                    <Image className='h-20 w-20 md:h-35 md:w-35 object-cover absolute bottom-0' src={manNew} height={100} width={100} alt='me'></Image>
+                }
               </div>
               <div className='w-20 md:w-35'>
                 <h1 className='text-center text-white'>{data[1].name}</h1>
@@ -101,7 +118,10 @@ export default function page() {
                     <div className='relative top-2'><Star w={40}></Star></div> */}
                   </div>
                 </div>
-                <Image className='h-20 w-20 md:h-35 md:w-35 object-cover absolute bottom-0' src={manNew} height={500} width={500} alt='me'></Image>
+                {
+                  data[0]?.photo ? <Image className='h-20 w-20 md:h-35 md:w-35 object-contain absolute bottom-0' src={data[0].photo} height={100} width={100} alt='me'></Image> :
+                    <Image className='h-20 w-20 md:h-35 md:w-35 object-cover absolute bottom-0' src={manNew} height={100} width={100} alt='me'></Image>
+                }
               </div>
               <div className='w-20 md:w-35'>
                 <h1 className='text-center text-white'>{data[0].name}</h1>
@@ -117,7 +137,10 @@ export default function page() {
                     {/* <div><Star w={40}></Star></div> */}
                   </div>
                 </div>
-                <Image className='h-20 w-20 md:h-35 md:w-35 object-cover absolute bottom-0' src={manNew} height={1000} width={1000} alt='me'></Image>
+                {
+                  data[2]?.photo ? <Image className='h-20 w-20 md:h-35 md:w-35 object-contain absolute bottom-0' src={data[2].photo} height={100} width={100} alt='me'></Image> :
+                    <Image className='h-20 w-20 md:h-35 md:w-35 object-cover absolute bottom-0' src={manNew} height={100} width={100} alt='me'></Image>
+                }
               </div>
               <div className='w-20 md:w-35'>
                 <h1 className='text-center text-white'>{data[2].name}</h1>
@@ -131,38 +154,45 @@ export default function page() {
 
 
         {/* FOR OTHERS PERSON */}
-        <section className='mt-40'>
+        <section className='mt-40 mb-10'>
 
-          <div className="overflow-x-auto">
-            <table className="table table-zebra w-full">
-              {/* head */}
-              <thead className='border-white border text-white'>
+          <div className="overflow-x-auto w-full p-2">
+            <table className="min-w-full text-center table-fixed">
+              <thead className="border-white border text-white">
                 <tr>
-                  <th>Position</th>
-                  <th>Image</th>
-                  <th>Name</th>
-                  <th>Score</th>
+                  <th className="w-32 py-4 px-6">Position</th>
+                  <th className="w-48 py-4 px-6">Image</th>
+                  <th className="w-72 py-4 px-6">Name</th>
+                  <th className="w-32 py-4 px-6">Score</th>
                 </tr>
               </thead>
+
               <tbody>
-                {
-                  data.slice(3).map((score: any, idx: any) => {
-                    return (
-                      <tr className={`${idx % 2 == 0 ? "bg-[#00960c34]" : ""} text-white text-center border`} key={idx}>
-                        <td className='py-4'>{idx + 4}</td>
-                        <td>NO Image</td>
-                        <td>{score.name}</td>
-                        <td>{score.score}</td>
-                      </tr>
-                    )
-                  })
-                }
+                {data.slice(3).map((score: any, idx: number) => (
+                  <tr
+                    key={idx}
+                    className={`${idx % 2 === 0 ? "bg-[#00960c34]" : ""} text-white border`}
+                  >
+                    <td className="py-4 px-6">{idx + 4}</td>
+                    <td className="px-6 flex justify-center">
+                      {
+                        score.photo ? <Image className='object-contain h-20 w-20' height={100} width={100} alt={score.name} src={score.photo}></Image> : "No Image"
+                      }
+                    </td>
+                    <td className="px-6">{score.name}</td>
+                    <td className="px-6">{score.score}</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
 
+
+
         </section>
       </div>
+
+      <Footer></Footer>
     </div>
   )
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='mb-5'>
+        <h1 className='text-white text-xs text-center'>&copy; 2026 SU-31M1. All rights reserved.</h1>
+        <p className='text-white text-xs text-center'>Md. Emran Ali</p>
+    </div>
+  )
+}
